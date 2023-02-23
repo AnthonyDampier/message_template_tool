168e8dfbf01267359cb7766e98f172f875edc6d1
AnthonyDampier <Dampieranthony2@gmail.com>
2023-02-23 12:02:54 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Message Template Tool</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
