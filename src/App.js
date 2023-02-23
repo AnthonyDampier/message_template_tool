289aa9dec246979b92b4b2ba433d3bea1bd8a919
AnthonyDampier <Dampieranthony2@gmail.com>
2023-02-23 13:19:32 -0600

    create getCurrentTime function and organized

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import * as React from 'react';
 import {} from "@mui/material";
 
-// imported data from asset folder
-import GuestData from './assets/Guests.json';
-import CompaniesData from './assets/Companies.json';
-import Messages from './assets/messages.json';
+// data imported from asset/data folder
+import guestData from './assets/data/Guests.json';
+import companiesData from './assets/data/Companies.json';
+import messageTemplates from './assets/data/MessageTemplates.json';
+import timeBasedGreeting from './assets/data/TimeBasedGreetings.json';
+
+// tools import from asset/tool folder
+import getCurrentTime from './assets/tools/GetCurrentTime';
+
+//components
+import UserInterface from './components/UserInterface'
 
 export default function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Message Template Tool</h1>
       </header>
-      <p>{JSON.stringify(GuestData)}</p>
-      <p>{JSON.stringify(CompaniesData)}</p>
-      <p>{JSON.stringify(Messages)}</p>
+      <UserInterface 
+        guestData={guestData} 
+        companiesData={companiesData} 
+        messageTemplates={messageTemplates} 
+        timeBasedGreetings={timeBasedGreeting}
+        getCurrentTime={getCurrentTime}
+      />
     </div>
   );
 }
